18381d90dfd377e5c295754f9e50625c8998850b
Freddy Mendoza <frmendozat@uni.pe>
2020-12-05 15:28:35 -0500

    first commit

---
+++ src/main/java/com/mitocode/MediappBackend01Application.java
@@ -0,0 +1,13 @@
+package com.mitocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediappBackend01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediappBackend01Application.class, args);
+    }
+
+}
